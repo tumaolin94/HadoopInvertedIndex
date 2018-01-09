d19a5eed49dc5966474ff92fd9d17ae9c08ad5c6
Maolin Tu <524561223@qq.com>
2018-01-08 17:35:09 -0800

    adding docID2Name function

--- src/code/maolin/InvertIndex.java
+++ src/code/maolin/InvertIndex.java
@@ -1,11 +1,17 @@
 package code.maolin;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
+import java.net.URI;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.regex.Pattern;
 
+import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.util.StringUtils;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 import org.apache.hadoop.mapreduce.Job;
@@ -13,6 +19,7 @@ import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.FileSplit;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
@@ -22,7 +29,7 @@ import org.apache.log4j.Logger;
 public class InvertIndex extends Configured implements Tool {
 
   private static final Logger LOG = Logger.getLogger(InvertIndex.class);
-  private HashMap<String, String> DocID2Name = new HashMap<>();
+  
   public static void main(String[] args) throws Exception {
     int res = ToolRunner.run(new InvertIndex(), args);
     System.exit(res);
@@ -57,8 +64,44 @@ public class InvertIndex extends Configured implements Tool {
     private final static IntWritable two = new IntWritable(2);
     private Text word = new Text();
     private long numRecords = 0;    
+    private String input;
+    private HashMap<String, String> DocID2Name = new HashMap<>();
     private static final Pattern WORD_BOUNDARY = Pattern.compile("\\s*\\b\\s*");
     private static final Pattern DOCUMENT_BOUNDARY = Pattern.compile("\\t");
+    
+    protected void setup(Mapper.Context context)
+            throws IOException,
+            InterruptedException {
+          if (context.getInputSplit() instanceof FileSplit) {
+            this.input = ((FileSplit) context.getInputSplit()).getPath().toString();
+          } else {
+            this.input = context.getInputSplit().toString();
+          }
+          Configuration config = context.getConfiguration();
+          if (config.getBoolean("invertIndex.map.patterns", false)) {
+            URI[] localPaths = context.getCacheFiles();
+            LOG.info("localPaths: " + localPaths[0]);
+            parseSkipFile(localPaths[0]);
+          }
+        }
+
+        private void parseSkipFile(URI patternsURI) {
+          LOG.info("Added file to the distributed cache: " + patternsURI);
+          try {
+            BufferedReader fis = new BufferedReader(new FileReader(new File(patternsURI.getPath()).getName()));
+            String pattern;
+            while ((pattern = fis.readLine()) != null) {
+            	String[] line = pattern.split(",");
+            	System.out.println(line[0]+" is "+line[1]);
+            	LOG.info(line[0]+" is "+line[1]);
+            	DocID2Name.put(line[0], line[1].substring(0, line[1].length()-4));
+            }
+          } catch (IOException ioe) {
+            System.err.println("Caught exception while parsing the cached file '"
+                + patternsURI + "' : " + StringUtils.stringifyException(ioe));
+          }
+        }
+    
     public void map(LongWritable offset, Text lineText, Context context)
         throws IOException, InterruptedException {
       String line = lineText.toString();
@@ -67,7 +110,7 @@ public class InvertIndex extends Configured implements Tool {
 //      System.out.println("Document id: "+ test[0]);
 //      System.out.println("Document text: "+ test[1]);
       String documentId = test[0];
-      Text docid = new Text(documentId);
+      Text docid = new Text(DocID2Name.get(documentId));
       Text currentWord = new Text();
       for (String word : WORD_BOUNDARY.split(test[1])) {
         if (word.isEmpty()) {
